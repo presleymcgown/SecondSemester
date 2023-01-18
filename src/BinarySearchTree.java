917ac8411737f0041f2f03e3314a1cf013b6f95b
presley <presley.mcgown@icloud.com>
2023-01-18 09:11:16 -0700

    BST part two work

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -86,30 +86,79 @@ public class BinarySearchTree<T extends Comparable<T>> implements Tree<T> {
 
     private Node BTSInsert(Node root, T data){
 
+
         if(root == null){
 
             return new Node(data);
 
-        }else{
-
-            if(data.compareTo(root.data) < 0) {
-
-                root = root.left;
-                return BTSInsert(root, data);
-
-            }else{
-
-                root = root.right;
-                return BTSInsert(root, data);
-
-            }
         }
+
+
+        if(data.compareTo(root.data) < 0) {
+
+            root.left = BTSInsert(root.left, data);
+
+        }else if(data.compareTo(root.data) > 0){
+
+            root.right = BTSInsert(root.right, data);
+
+        }
+
+        return root;
 
     }
 
     private Node BTSDelete(Node root, T data){
 
-        
+        if(root == null){
+
+            return null;
+
+        }else{
+
+            if(data.compareTo(root.data) < 0){
+
+                // less than
+                root.left = BTSDelete(root.left, data);
+
+            }else if(data.compareTo(root.data) > 0){
+
+                // greater than
+                root.right = BTSDelete(root.right, data);
+
+            }else{
+
+                // equal to
+                if(root.right != null && root.left == null){
+
+                    // left child null
+                    return root.right;
+
+                }else if(root.left != null && root.right == null){
+
+                    // right child null
+                    return root.left;
+
+                }else if(root.left != null && root.right != null){
+
+                    // neither null
+
+                    root.data = getMin(root);
+
+                    root.left = BTSDelete(root.left, root.data);
+
+                }else{
+
+                    //both null
+                    return null;
+
+                }
+
+            }
+
+        }
+
+        return null;
 
     }
 
@@ -119,30 +168,29 @@ public class BinarySearchTree<T extends Comparable<T>> implements Tree<T> {
 
             return false;
 
-        }else{
+        }
 
-            if(target == root){
+        if(target == root.data){
 
-                return true;
-
-            }else{
-
-                if(target.compareTo(root.data) < 0){
-
-                    root = root.left;
-
-                    return BTSContains(root, target);
-
-                }else{
-
-                    root = root.right;
-                    return BTSContains(root, target);
-
-                }
-
-            }
+            return true;
 
         }
+
+        if(target.compareTo(root.data) < 0){
+
+            root = root.left;
+            BTSContains(root, target);
+
+        }else{
+
+            root = root.right;
+            BTSContains(root, target);
+
+        }
+
+
+
+        return false;
 
     }
 
@@ -157,5 +205,6 @@ public class BinarySearchTree<T extends Comparable<T>> implements Tree<T> {
         root = new Node(data);
 
     }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -7,24 +7,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] grades = {60, 70, 75, 80, 85, 90, 95};
-        int target1 = 80;
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
 
-        BinarySearch.q1(grades, target1);
+        tree.insert(5);
+        tree.insert(1);
+        tree.insert(10);
 
-        double[] atomicWeights = {1.0079, 4.0026, 6.941, 6.941, 9.0122, 10.811, 12.0107, 12.0107, 14.0067};
-        double target2 = 12.0107;
+        System.out.println(tree.contains(8));
+        System.out.println(tree.contains(5));
+        System.out.println(tree.contains(1));
+        System.out.println(tree.contains(10));
 
-        BinarySearch.q2(atomicWeights, target2);
-
-        short[] dates = {1492, 1776, 1803, 1861, 1861, 1861, 1945};
-        short target3 = 1861;
-
-        BinarySearch.q3(dates, target3);
-
-        String[] patientNames = {"Brown", "Johnson", "Johnson", "Johnson", "Johnson", "Jones", "Smith", "Williams"};
-        String target4 = "Johnson";
-        BinarySearch.q4(patientNames, target4);
 
     }
 
